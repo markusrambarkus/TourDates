b114730ad88a5228292fcdcc96b871d090312fa5
Mark Saunders <Mark.Saunders@condecosoftware.com>
2018-12-13 11:17:36 +0000

    routing and route guards / auth protection

---
+++ TourDates-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  /**
+   *
+   */
+  constructor(private alertify: AlertifyService,
+              private authService: AuthService,
+              private router: Router) {}
+
+  canActivate(): boolean {
+
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('You shall not pass!!!');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
